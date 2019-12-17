f9e154681f2ea45931736432b3b9d3f968ba2f70
吴圣炜 <zuoer@huored.com>
2019-12-17 16:32:36 +0800

    完善查询sql

--- dal/src/main/java/com/zuoer/sofa/blog/dal/mapper/UserMapper.java
+++ dal/src/main/java/com/zuoer/sofa/blog/dal/mapper/UserMapper.java
@@ -1,16 +1,25 @@
 package com.zuoer.sofa.blog.dal.mapper;
 
 import com.zuoer.sofa.blog.dal.entity.UserDO;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author James
  */
 public interface UserMapper extends CommonMybatisMapper<UserDO,Integer> {
-	
+
+	String ALL_COLUMN_STR="id, username, password, birth_date, nickname, email, github, qq, wecaht, avatar, gmt_create, gmt_modified, hobby, motto, md_info";
+
+
 	/**
 	 * 
 	 * @param userName
 	 * @return
 	 */
+	@Select({
+			"SELECT ",
+			ALL_COLUMN_STR,
+			" FROM tag WHERE `userName` = #{userName}"
+	})
 	public UserDO selectByUserName(String userName);
 }
